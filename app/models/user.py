ae83ca860cc71ce7a31a525325201e5ad7e7d1fa
Ritikrak <rritik345@gmail.com>
2026-08-05 19:11:50 +0530

    creating the user management module

---
+++ app/models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+
+from app.db.base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    role = Column(String, nullable=False)
