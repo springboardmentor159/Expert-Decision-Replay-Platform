ab4cd6f3aa500bd427aba0c9b49a09275debd326
Mohana Geethika <geethika.sanagapalli324@gmail.com>
2026-08-05 19:24:48 +0530

    Implemented CRUD operations

--- Expert_Decision_Replay/core/config.py
+++ Expert_Decision_Replay/core/config.py
@@ -1,12 +1,14 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
-    APP_NAME: str = "Expert Decision Replay Platform"
-    DATABASE_URL: str
+    app_name: str = "Expert Decision Replay Platform"
+    database_url: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
         extra="ignore"
     )
+
 
 settings = Settings()
