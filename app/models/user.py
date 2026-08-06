37d96ff893a8cada05c8f52c55f70d55bcff70ad
Jamuna-2006 <ranivjamuna8@gmail.com>
2026-08-06 23:19:35 +0530

    Completed user CRUD

---
+++ app/models/user.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String
+from app.db.base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    full_name = Column(String, nullable=False)
+
+    email = Column(String, unique=True, nullable=False)
+
+    role = Column(String, nullable=False)
+
+    password = Column(String, nullable=False)
---
+++ app/schemas/user.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr
+
+
+class UserCreate(BaseModel):
+    full_name: str
+    email: EmailStr
+    role: str
+    password: str
+
+
+class UserUpdate(BaseModel):
+    full_name: Optional[str] = None
+    email: Optional[EmailStr] = None
+    role: Optional[str] = None
+    password: Optional[str] = None
+
+
+class UserResponse(BaseModel):
+    id: int
+    full_name: str
+    email: EmailStr
+    role: str
+
+    class Config:
+        from_attributes = True
