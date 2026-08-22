1cf579d52ad00e7664298c82ecb7dd1f8d109733
Jamuna-2006 <ranivjamuna8@gmail.com>
2026-08-22 21:56:37 +0530

    real business system

--- app/schemas/decision.py
+++ app/schemas/decision.py
@@ -1,14 +1,36 @@
 from datetime import datetime
+from enum import Enum
 
 from pydantic import BaseModel
 
 
+# CREATE DECISION
 class DecisionCreate(BaseModel):
     title: str
     problem_statement: str
     category: str
 
 
+# UPDATE DECISION
+class DecisionUpdate(BaseModel):
+    title: str
+    problem_statement: str
+    category: str
+
+
+# DECISION STATUS
+class DecisionStatus(str, Enum):
+    Draft = "Draft"
+    Approved = "Approved"
+    Rejected = "Rejected"
+
+
+# UPDATE STATUS
+class DecisionStatusUpdate(BaseModel):
+    status: DecisionStatus
+
+
+# RESPONSE
 class DecisionResponse(BaseModel):
     id: int
     title: str
